f6a7b7614ed18468fc3b7ea6ef3c5b396667eaf3
Igor Tarasenko <tarasenko.igr@gmail.com>
2019-03-05 13:48:21 +0200

    add new countdown timer

--- temp/js/main.js
+++ temp/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    $(function () {
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc2-days').text('días');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc20-days').text('días');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc200-days').text('días');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc2-hours').text('en punto');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc200-hours').text('en punto');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc20-hours').text('en punto');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc2-minutes').text('minutos');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc20-minutes').text('minutos');
+        $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc200-minutes').text('minutos');
+    });
     $("a.topLink").click(function() {
         $("html, body").animate({
             scrollTop: $($(this).attr("href")).offset().top + "px"
@@ -22,3 +33,17 @@ $(document).ready(function () {
         ]
     });
 });
+function replaceText() {
+    var days = $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc2-days');
+   /* var hours = $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc2-hours');
+    var minutes = $('#timer-text-2f3c04bf9684bc0ba46883b86395ffc2-minutes');*/
+    var text = days.html();
+    /*var text2 = hours.html();
+    var text3 = minutes.html();*/
+    text = text.replace('дней','días');
+    /*text2 = text2.replace('часов','en punto');
+    text3 = text3.replace('минут','minutos');*/
+    days.html(text);
+    /*hours.html(text2);
+    minutes.html(text3);*/
+}
